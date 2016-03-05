b3d22733895b8e1e47920a8c486f631ba17033c3
Clodial <maravillamatthew@gmail.com>
2016-03-04 20:51:55 -0500

    Employee Settings Branch

--- app/view/pageActSettings.php
+++ app/view/pageActSettings.php
@@ -16,16 +16,63 @@ class pageActSettings extends model\pageTemplate{
 
 		if(isset($_SESSION['actType']) && (isset($_SESSION['username']))){
 
+			// User update section
 
+			$this->updateInfo();
+
+			if($_SESSION['actType'] == 'customer'){
+
+				//customer settings
+
+				$this->payAdd();
+
+			}else{
+
+				//employee settings
+
+				$this->bookAdd();
+
+			}
 
 		}else{
 
 			echo 'Error: You are not supposed to be on this page';			
 
+
 		}
 
 	}
+
+	public function updateInfo(){
+
+		/**
+		*
+		*
+		*
+		**/
+
+	}
 	
+	public function payAdd(){
+
+		/**
+		*
+		*
+		*
+		**/
+
+	}
+
+	public function bookAdd(){
+
+		/**
+		*
+		*
+		*
+		**/
+
+	}
+
 } 
 
 ?>

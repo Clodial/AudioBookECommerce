189469cbb299dcac688a6559cb8ffb8f5b91ddf0
Clodial <maravillamatthew@gmail.com>
2016-03-07 18:21:48 -0500

    added item page stuff

--- app/view/pageCart.php
+++ app/view/pageCart.php
@@ -4,7 +4,32 @@ use app\model as model;
 
 class pageCart extends model\pageTemplate{
 
-	
+	/**
+	*
+	*	Page that shows all of the items in a user's current cart
+	*
+	**/
+
+	private $db;
+
+	public function __construct($db){
+
+		$this->db = $db;
+
+	}
+
+	public function getBody(){
+
+		/**
+		*
+		*	1) Display Cart Items (show the total price, and show the items in a page)
+		*	2) Allow user to select payment option or update cart (ie remove specific items from cart)
+		*	3) When a user chooses to purchase items, clear out a cart and add that info to a new order with item order details of each item for users to look up in an order-lookup
+		*	4) Clear out payment options if there is nothing to purchase
+		*
+		**/
+		
+	}
 
 }
 
--- app/view/pageItem.php
+++ app/view/pageItem.php
@@ -6,6 +6,56 @@ class pageItem extends model\pageTemplate{
 	
 	//contains the data leading to each individual item page
 
+	private $db;
+
+	public function __construct($db){
+
+		$this->db = $db;
+
+	}
+
+	public function getBody(){
+
+		//show item data
+		$bImage;
+		$bName;
+		$bAuth;
+		$bGenre;
+		$bPrice;
+		$bDesc;
+		if(isset($_REQUEST['itemName'])){
+			try{
+				$stmt = $this->db->prepare('
+					select * from inventory where item_name = :name
+				');
+				$stmt->bindParam(':name',$bName);
+				if($stmt->execute()){
+					while($data = $stmt->fetch()){
+						$bImage = $data[6];
+						$bName = $data[5];
+						$bAuth = $data[3];
+						$bGenre = $data[1];
+						$bPrice = $data[4];
+						$bDesc = $data[3];
+					}
+				}
+			}catch(Exception $e){
+				
+			}
+
+			$this->buildItemPage($bImage,$bName,$bAuth,$bGenre,$bPrice,$bDesc);
+
+		}else{
+			
+		}
+	}
+
+	public function buildItemPage($img,$name,$auth,$genre,$price,$desc){
+
+		
+
+	}
+
 } 
 
 ?>

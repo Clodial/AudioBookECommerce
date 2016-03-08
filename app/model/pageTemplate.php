169fb00c83b17b4b4ae31d3d2a6b5c705c85ad38
ajm27 <ajm27@njit.edu>
2016-03-07 22:23:33 -0500

    updating the dB doesn't work, page loads

--- app/model/pageTemplate.php
+++ app/model/pageTemplate.php
@@ -86,6 +86,7 @@ abstract class pageTemplate{
 		echo '	<ul class="nav navbar-nav nav-right">';
 		echo '  	<li><button class="" type="submit" name="page" value="pageLogout">Logout</button</li>';
 		echo '  	<li><button class="" type="submit" name="page" value="pageCart">Cart</button></li>';
+		echo '  	<li><button class="" type="submit" name="page" value="pagePayment">Add Payment</button></li>';
 		echo '  </ul>';
 		echo '</form>';
 
--- app/view/pagePayment.php
+++ app/view/pagePayment.php
@@ -21,103 +21,102 @@ class pagePayment extends model\pageTemplate
 		* @brief -> creates the Add Payment Method functionality for the page
 		*
 		**/
-			if(!isset($_SESSION['username']))
-			{
+		if(!isset($_SESSION['username']))
+		{
 
-				echo '<h2>Please Sign In To Add A New Payment</h2>';
-				echo '<form method="get">';
-				echo '	<button class="" type="submit" name="page" value="pageLogin">Sign In</button>';
-				echo '</form>';
+			echo '<h2>Please Sign In To Add A New Payment</h2>';
+			echo '<form method="get">';
+			echo '	<button class="" type="submit" name="page" value="pageLogin">Sign In</button>';
+			echo '</form>';
+		}
+		else
+		{
+
+			if((isset($_REQUEST['noc']) && isset($_REQUEST['billad']) && isset($_REQUEST['ccnum']) && isset($_REQUEST['expdate']) && isset($_REQUEST['phNum'])))
+			{
+				/**
+				*
+				*	database additions
+				*
+				**/
+
+				$id = $_SESSION['account_id'];
+				$noc = $_REQUEST['noc'];
+				$billad = $_REQUEST['billad'];
+				$ccnum = $_REQUEST['ccnum'];
+				$expdate = $_REQUEST['expdate'];
+				$phNum = $_REQUEST['phNum'];
+
+				try
+				{
+					$this->db->beginTransaction();
+
+					$stmt = $this->db->prepare('
+							select * from customer_payment where account_id = :id;
+							');
+
+					$stmt->bindParam(':id', $id);
+					$stmt->execute();
+					$stmt = $this->db->prepare('
+								insert into customer_payment(account_ID , name_on_card, billing_address, card_number, expDate, phNum) 
+								values(
+									:id, 
+									:noc, 
+									:billad, 
+									:ccnum, 
+									:expDate, 
+									:phNum);');
+							$stmt->bindParam(':id', $id);
+							$stmt->bindParam(':noc', $noc);
+							$stmt->bindParam(':billad', $billad);
+							$stmt->bindParam(':ccnum', $ccnum);
+							$stmt->bindParam(':expDate', $expDate);
+							$stmt->bindParam(':phNum', $phNum);
+							$stmt->execute(); // like a git add
+							echo '<h3>Payment Addition Successful</h3>';
+							
+							$this->db->commit(); // like a git commit
+
+							echo '<form method="get">';
+							echo ' 	<button type="submit" name="page" value="pageCart">To Checkout</button>';
+							echo '</form>';
+				}
+				catch(Exception $e)
+				{
+					$this->db->rollBack(); 	// in case of any errors, this gets called and anything before the db->commit doesn't happen
+					echo '<h3>User Information is incorrect</h3>';
+					$this->newPayment();
+				}
 			}
 			else
 			{
-
-				if((isset($_REQUEST['noc']) && isset($_REQUEST['billad']) && isset($_REQUEST['ccnum']) && isset($_REQUEST['rePass']) && isset($_REQUEST['expdate']) && isset($_REQUEST['number'])) && $_REQUEST['password'] == $_REQUEST['rePass'])
-				{
-					/**
-					*
-					*	database additions
-					*
-					**/
-
-					$id = $_SESSION['account_id'];
-					$noc = $_REQUEST['noc'];
-					$billad = $_REQUEST['billad'];
-					$ccnum = $_REQUEST['ccnum'];
-					$expdate = $_REQUEST['expdate'];
-					$phNum = $_REQUEST['number'];
-
-					try
-					{
-						$this->db->beginTransaction();
-
-						$stmt = $this->db->prepare('
-								select * from payment where account_id = :id;
-								');
-
-						$stmt->bindParam(':id', $id);
-						$stmt->execute();
-						$stmt = $this->db->prepare('
-									insert into customer_payment(account_ID , name_on_card, billing_address, card_number, expDate, phNum) 
-									values(
-										:id, 
-										:nameCard, 
-										:billad, 
-										:cardNum, 
-										:expDate, 
-										:phNum);');
-								$stmt->bindParam(':id', $id);
-								$stmt->bindParam(':nameCard', $noc);
-								$stmt->bindParam(':billad', $billad);
-								$stmt->bindParam(':cardNum', $ccnum);
-								$stmt->bindParam(':expDate', $expDate);
-								$stmt->bindParam(':phNum', $phNum);
-								$stmt->execute(); // like a git add
-								echo '<h3>Payment Addition Successful</h3>';
-								
-								$this->db->commit(); // like a git commit
-
-								echo '<form method="get">';
-								echo ' 	<button type="submit" name="page" value="pageLogin">Sign In</button>';
-								echo '</form>';
-					}
-					catch(Exception $e)
-					{
-						$this->db->rollBack(); 	// in case of any errors, this gets called and anything before the db->commit doesn't happen
-						echo '<h3>User Information is incorrect</h3>';
-						$this->newPayment();
-					}
-				}
-				else
-				{
-					$this->newPayment();
-				}
+				$this->newPayment();
+			}
 		}
+	}
 
-		public function newPayment()
-		{
+	 function newPayment()
+	{
 
-			echo '
-					<div id="paymentBody">
-						<h2>Add A New Payment</h2>
-						<form method="get">
-							<label>Name on Card</label>
-							<input type="text" name="noc" required><br>
-							<label>Billing Address</label>
-							<input type="text" name="billad" required></br>
-							<label>Credit Card Number</label>
-							<input type="text" name="ccnum" required></br>
-							<label>Expiration Date</label>
-							<input type="text" name="expdate" required></br>
-							<label>Phone Number</label>
-							<input type="text" name="number" required></br>
-							</select></br>
-							<button type="submit" name="page" value="pageRegister">Register Card</button>
-						</form>
-					</div>
-					';
-
-		}
+		echo '
+				<div id="paymentBody">
+					<h2>Add A New Payment</h2>
+					<form method="get">
+						<label>Name on Card</label>
+						<input type="text" name="noc" required><br>
+						<label>Billing Address</label>
+						<input type="text" name="billad" required></br>
+						<label>Credit Card Number</label>
+						<input type="text" name="ccnum" required></br>
+						<label>Expiration Date</label>
+						<input type="text" name="expdate" required></br>
+						<label>Phone Number</label>
+						<input type="text" name="phNum" required></br>
+						</select></br>
+						<button type="submit" name="page" value="pageRegister">Register Card</button>
+					</form>
+				</div>
+				';
 
 	}
 }

5e85b7d91e2605bfd98d48427990255e5f6d583e
Clodial <maravillamatthew@gmail.com>
2016-03-07 23:14:10 -0500

    Working cart adding features

--- app/view/pageBrowse.php
+++ app/view/pageBrowse.php
@@ -17,17 +17,27 @@ class pageBrowse extends model\pageTemplate
 
 		try{
 
-			$rowcount = 0;
-			$stmt = $this->db->prepare('select * from inventory where item_ID <= 3');
-
-			if($stmt->execute()){
-
-				while($data = $stmt->fetch()){
-					echo ''.$data[5].'-  Book ID:'.$data[0].' Author:'.$data[2].' Price: $'.$data[4].'<br>';
-					$rowcount++;
+			if(isset($_REQUEST['genre'])){
+				$stmt = $this->db->prepare('select * from inventory where item_genre = :gen');
+			}else{
+				$stmt = $this->db->prepare('select * from inventory');
+				if($stmt->execute()){
+					while($data = $stmt->fetch()){
+						echo '
+						<div class="smItemBody">
+							<div class="miniImg"><img src="data:image/jpeg;base64,'.base64_encode( $data[6] ).'" height=100px width=100px/></div>
+							<div class="itemInfo">
+								<form method="get">
+									<input type="hidden" name="itemName" value="' . $data[5] . '">
+									<button type="submit" name="page" value="pageItem">'. $data[5] .'</button>
+								</form>
+								<h6>By: ' . $data[2] .'</h6>
+							</div>
+						</div> 
+						';
+					}
 				}
 			}
-
 		}catch(Exception $e){
 			echo'errrrrrrrrrrrror';
 		}
--- app/view/pageItem.php
+++ app/view/pageItem.php
@@ -20,11 +20,11 @@ class pageItem extends model\pageTemplate{
 		$bImage;
 		$bName;
 		$bAuth;
-		$bGenre;
-		$bPrice;
-		$bDesc;
+		$bGenre = null;
+		$bPrice = null;
+		$bDesc = null;
 		if(isset($_REQUEST['aCart']) && isset($_SESSION['username']) && isset($_SESSION['actType']) && $_SESSION['actType'] == 'customer'){
-
+			echo $_REQUEST['aCart'];
 			try{
 				$item;
 				$account;
@@ -46,25 +46,33 @@ class pageItem extends model\pageTemplate{
 				$stmt = $this->db->prepare('
 					insert into cart_item(item_ID, account_ID) values(:item, :act);
 				');
+				$stmt->bindParam(':item',$item);
+				$stmt->bindParam(':act',$account);
+				$stmt->execute();
 				$this->db->commit();
-				echo 'Book Successfully in Cart'
+				echo 'Book Successfully in Cart';
 			}catch(Exception $e){
 				$this->db->rollBack();
 				echo 'There was a problem.';
 			}
 
+		}else if(isset($_SESSION['actType']) && $_SESSION['actType'] == 'employee'){
+
+			echo 'Employees cannot buy stuff';
+
 		}
+
 		if(isset($_REQUEST['itemName'])){
 			try{
 				$stmt = $this->db->prepare('
 					select * from inventory where item_name = :name
 				');
-				$stmt->bindParam(':name',$bName);
+				$stmt->bindParam(':name',$_REQUEST['itemName']);
 				if($stmt->execute()){
 					while($data = $stmt->fetch()){
 						$bImage = $data[6];
 						$bName = $data[5];
-						$bAuth = $data[3];
+						$bAuth = $data[2];
 						$bGenre = $data[1];
 						$bPrice = $data[4];
 						$bDesc = $data[3];
@@ -82,7 +90,7 @@ class pageItem extends model\pageTemplate{
 	}
 
 	public function buildItemPage($img,$name,$auth,$genre,$price,$desc){
-		$gName;
+		$gName = null;
 		try{
 			$stmt = $this->db->prepare('
 				select genre_name from genre where genre_ID = :gen
@@ -101,7 +109,7 @@ class pageItem extends model\pageTemplate{
 		<div class="itemBody">
 			<div class="itemTop">
 				<div class="itemPic">
-					<img src="data:image/jpeg;base64,'.base64_encode( $img ).'"/>
+					<img src="data:image/jpeg;base64,'.base64_encode( $img ).'" height=200px width=200px/>
 				</div>
 				<div class="itemInfo">
 					<h2>'.$name.'</h2><br>
@@ -120,7 +128,7 @@ class pageItem extends model\pageTemplate{
 				<input type="hidden" name="aCart" value="'.$name.'">
 				<button type="submit" name="page" value="pageItem">Buy Item</button>
 			</form>
-		</div>'
+		</div>';
 
 	}
 

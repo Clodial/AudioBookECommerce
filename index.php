4b227bd2e4ebe9734a6d15b4fbbadb2df06dd6af
Clodial <maravillamatthew@gmail.com>
2016-04-26 18:55:33 -0400

    yolo

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ try{
 use app\model as Model;
 use app\view as View;
 $main = new Model\main($db);
-echo json_decode(bookAPIuse("https://web.njit.edu/~cmn6/IT490/testApi.php", 'searchByOrd', 6, 0));
+print_r(json_decode(bookAPIuse("https://web.njit.edu/~cmn6/IT490/testApi.php", 'searchByOrd', 6, 0)));
 //echo bookAPIuse("https://web.njit.edu/~cmn6/IT490/testApi.php", 'checkAvailable', 0);
 //echo bookAPIuse("http://localhost/AudioBookIT490/api/v1/bookApi.php", 'updateOrder', 10);
 //echo bookAPIuse("http://localhost/AudioBookIT490/api/v1/bookApi.php", 'addressData', 0);

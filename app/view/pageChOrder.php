bac2cd16f4dfbaaaabe0239b629ad686738dab42
Clodial <maravillamatthew@gmail.com>
2016-04-27 00:28:25 -0400

    adljhasd

--- app/view/pageChOrder.php
+++ app/view/pageChOrder.php
@@ -195,7 +195,7 @@ class pageChOrder extends model\pageTemplate{
 							$stmt->bindParam(':ord', $orderId);
 							$stmt->execute();
 							$this->db->commit();
-							bookAPIuse("https://web.njit.edu/~cmn6/IT490/testApi.php", 'getFlight', $orderId, $_REQUEST['flightNum']);
+							bookAPIuse("https://web.njit.edu/~cmn6/IT490/testApi.php", 'addItem', $orderId, $_REQUEST['flightNum']);
 						}catch(Exception $e){
 							$this->db->rollBack();
 						}
